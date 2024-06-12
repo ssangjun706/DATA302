6b42d602e887972ff41fae8e4a5c1e32299ebbe9
ssangjun706 <ssangjun706@naver.com>
2024-06-13 01:41:14 +0900

    [Add] S-GAN Code

---
+++ utils.py
@@ -0,0 +1,215 @@
+import os
+import time
+import torch
+import numpy as np
+import inspect
+from contextlib import contextmanager
+import subprocess
+
+
+def int_tuple(s):
+    return tuple(int(i) for i in s.split(','))
+
+
+def find_nan(variable, var_name):
+    variable_n = variable.data.cpu().numpy()
+    if np.isnan(variable_n).any():
+        exit('%s has nan' % var_name)
+
+
+def bool_flag(s):
+    if s == '1':
+        return True
+    elif s == '0':
+        return False
+    msg = 'Invalid value "%s" for bool flag (should be 0 or 1)'
+    raise ValueError(msg % s)
+
+
+def lineno():
+    return str(inspect.currentframe().f_back.f_lineno)
+
+
+def get_total_norm(parameters, norm_type=2):
+    if norm_type == float('inf'):
+        total_norm = max(p.grad.data.abs().max() for p in parameters)
+    else:
+        total_norm = 0
+        for p in parameters:
+            try:
+                param_norm = p.grad.data.norm(norm_type)
+                total_norm += param_norm**norm_type
+                total_norm = total_norm**(1. / norm_type)
+            except:
+                continue
+    return total_norm
+
+
+@contextmanager
+def timeit(msg, should_time=True):
+    if should_time:
+        torch.cuda.synchronize()
+        t0 = time.time()
+    yield
+    if should_time:
+        torch.cuda.synchronize()
+        t1 = time.time()
+        duration = (t1 - t0) * 1000.0
+        print('%s: %.2f ms' % (msg, duration))
+
+
+def get_gpu_memory():
+    torch.cuda.synchronize()
+    opts = [
+        'nvidia-smi', '-q', '--gpu=' + str(1), '|', 'grep', '"Used GPU Memory"'
+    ]
+    cmd = str.join(' ', opts)
+    ps = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = ps.communicate()[0].decode('utf-8')
+    output = output.split("\n")[0].split(":")
+    consumed_mem = int(output[1].strip().split(" ")[0])
+    return consumed_mem
+
+
+def get_dset_path(dset_name, dset_type):
+    _dir = os.path.dirname(__file__)
+    _dir = _dir.split("/")[:-1]
+    _dir = "/".join(_dir)
+    return os.path.join(_dir, 'datasets', dset_name, dset_type)
+
+
+def relative_to_abs(rel_traj, start_pos):
+    """
+    Inputs:
+    - rel_traj: pytorch tensor of shape (seq_len, batch, 2)
+    - start_pos: pytorch tensor of shape (batch, 2)
+    Outputs:
+    - abs_traj: pytorch tensor of shape (seq_len, batch, 2)
+    """
+    # batch, seq_len, 2
+    rel_traj = rel_traj.permute(1, 0, 2)
+    displacement = torch.cumsum(rel_traj, dim=1)
+    start_pos = torch.unsqueeze(start_pos, dim=1)
+    abs_traj = displacement + start_pos
+    return abs_traj.permute(1, 0, 2)
+
+import torch
+import random
+
+
+def bce_loss(input, target):
+    """
+    Numerically stable version of the binary cross-entropy loss function.
+    As per https://github.com/pytorch/pytorch/issues/751
+    See the TensorFlow docs for a derivation of this formula:
+    https://www.tensorflow.org/api_docs/python/tf/nn/sigmoid_cross_entropy_with_logits
+    Input:
+    - input: PyTorch Tensor of shape (N, ) giving scores.
+    - target: PyTorch Tensor of shape (N,) containing 0 and 1 giving targets.
+
+    Output:
+    - A PyTorch Tensor containing the mean BCE loss over the minibatch of
+      input data.
+    """
+    neg_abs = -input.abs()
+    loss = input.clamp(min=0) - input * target + (1 + neg_abs.exp()).log()
+    return loss.mean()
+
+
+def gan_g_loss(scores_fake):
+    """
+    Input:
+    - scores_fake: Tensor of shape (N,) containing scores for fake samples
+
+    Output:
+    - loss: Tensor of shape (,) giving GAN generator loss
+    """
+    y_fake = torch.ones_like(scores_fake) * random.uniform(0.7, 1.2)
+    return bce_loss(scores_fake, y_fake)
+
+
+def gan_d_loss(scores_real, scores_fake):
+    """
+    Input:
+    - scores_real: Tensor of shape (N,) giving scores for real samples
+    - scores_fake: Tensor of shape (N,) giving scores for fake samples
+
+    Output:
+    - loss: Tensor of shape (,) giving GAN discriminator loss
+    """
+    y_real = torch.ones_like(scores_real) * random.uniform(0.7, 1.2)
+    y_fake = torch.zeros_like(scores_fake) * random.uniform(0, 0.3)
+    loss_real = bce_loss(scores_real, y_real)
+    loss_fake = bce_loss(scores_fake, y_fake)
+    return loss_real + loss_fake
+
+
+def l2_loss(pred_traj, pred_traj_gt, loss_mask, random=0, mode='average'):
+    """
+    Input:
+    - pred_traj: Tensor of shape (seq_len, batch, 2). Predicted trajectory.
+    - pred_traj_gt: Tensor of shape (seq_len, batch, 2). Groud truth
+    predictions.
+    - loss_mask: Tensor of shape (batch, seq_len)
+    - mode: Can be one of sum, average, raw
+    Output:
+    - loss: l2 loss depending on mode
+    """
+    seq_len, batch, _ = pred_traj.size()
+    loss = (loss_mask.unsqueeze(dim=2) *
+            (pred_traj_gt.permute(1, 0, 2) - pred_traj.permute(1, 0, 2))**2)
+    if mode == 'sum':
+        return torch.sum(loss)
+    elif mode == 'average':
+        return torch.sum(loss) / torch.numel(loss_mask.data)
+    elif mode == 'raw':
+        return loss.sum(dim=2).sum(dim=1)
+
+
+def displacement_error(pred_traj, pred_traj_gt, consider_ped=None, mode='sum'):
+    """
+    Input:
+    - pred_traj: Tensor of shape (seq_len, batch, 2). Predicted trajectory.
+    - pred_traj_gt: Tensor of shape (seq_len, batch, 2). Ground truth
+    predictions.
+    - consider_ped: Tensor of shape (batch)
+    - mode: Can be one of sum, raw
+    Output:
+    - loss: gives the eculidian displacement error
+    """
+    seq_len, _, _ = pred_traj.size()
+    loss = pred_traj_gt.permute(1, 0, 2) - pred_traj.permute(1, 0, 2)
+    loss = loss**2
+    if consider_ped is not None:
+        loss = torch.sqrt(loss.sum(dim=2)).sum(dim=1) * consider_ped
+    else:
+        loss = torch.sqrt(loss.sum(dim=2)).sum(dim=1)
+    if mode == 'sum':
+        return torch.sum(loss)
+    elif mode == 'raw':
+        return loss
+
+
+def final_displacement_error(
+    pred_pos, pred_pos_gt, consider_ped=None, mode='sum'
+):
+    """
+    Input:
+    - pred_pos: Tensor of shape (batch, 2). Predicted last pos.
+    - pred_pos_gt: Tensor of shape (seq_len, batch, 2). Groud truth
+    last pos
+    - consider_ped: Tensor of shape (batch)
+    Output:
+    - loss: gives the eculidian displacement error
+    """
+    loss = pred_pos_gt - pred_pos
+    loss = loss**2
+    if consider_ped is not None:
+        loss = torch.sqrt(loss.sum(dim=1)) * consider_ped
+    else:
+        loss = torch.sqrt(loss.sum(dim=1))
+    if mode == 'raw':
+        return loss
+    else:
+        return torch.sum(loss)
